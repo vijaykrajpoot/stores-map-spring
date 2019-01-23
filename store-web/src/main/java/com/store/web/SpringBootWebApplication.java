87d5132c185e6956b93394fcc692c25c46c0ba84
Vijay Kumar Rajput <vijay.x.kumar@macys.com>
2019-01-23 00:15:27 -0800

    Removed version. Provided by parent/spring-boot-starter-parent

--- store-web/src/main/java/com/store/web/SpringBootWebApplication.java
+++ store-web/src/main/java/com/store/web/SpringBootWebApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages={"com.store.web","com.store.service"})
+@SpringBootApplication(scanBasePackages = { "com.store.web", "com.store.service" })
 //@ComponentScan({"com.store.web","com.store.service"})
 //@EnableAutoConfiguration
 //@Configuration
 //@EnableWebMvc
-public class SpringBootWebApplication extends SpringBootServletInitializer{
+public class SpringBootWebApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SpringBootWebApplication.class);
